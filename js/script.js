372e15930099b21df4d8399e44d7d40fc737ee44
Michael Wallert <michael.wallert@hotmail.com>
2016-05-04 21:00:02 -0700

    Added alphabet utility for user, started logic for letter check on the start of the game

--- js/script.js
+++ js/script.js
@@ -17,6 +17,18 @@ function createTableElement(numCols) {
   table.appendChild(row);
   return table;
 }
+//Creates table for the alphabet
+function createTableElements(numCols) {
+  var table = document.createElement('table');
+  var row = document.createElement('tr');
+  for(var c = 0; c<numCols; c++){
+      var cell = document.createElement('td');
+      cell.className += 'cells';
+      row.appendChild(cell);
+  }
+  table.appendChild(row);
+  return table;
+}
 
 //Function to insert the letter of the song name in each box, one letter at a time
 function insertLetter () {
@@ -26,14 +38,20 @@ function insertLetter () {
       if(letter[l]!==" "){
         letter[l] = letter[l].toUpperCase();
       }
-      document.getElementsByTagName('td')[l].innerHTML = letter[l];
+      document.getElementsByClassName('cell')[l].innerHTML = letter[l];
       //If the letter is a blank space, changes the background color to represent the blank space
       if(letter[l]===" "){
-        document.getElementsByTagName('td')[l].style.backgroundColor = "blue";
+        document.getElementsByClassName('cell')[l].style.backgroundColor = "blue";
       }
   }
 }
-
+//Inserts alphabet into the grid array
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",'O','P',"Q",'R','S','T','U','V','W','X','Y','Z'];
+function insertAlphabet (numCols) {
+  for(var l = 0; l<numCols; l++){
+      document.getElementsByClassName('cells')[l].innerHTML = alphabet[l];
+  }
+}
 //Target element for the table
 var wordGrid = document.getElementById('Word');
 
@@ -43,9 +61,51 @@ var setWord = createTableElement(randomSong.length);
 //Appends the table to the target element, sending the table to the dom
 wordGrid.appendChild(setWord);
 
+//Table element for the available letters grid
+var letterGrid = document.getElementById('Letters');
+var setLetters = createTableElements(26);
+letterGrid.appendChild(setLetters);
+
 //Inserts the letter into each element of the table
 insertLetter();
-
+insertAlphabet(26);
+//Function that hides and reveals the game word
+function wordHide (reveal) {
+  for(var i = 0; i < randomSong.length; i++){
+    document.getElementsByClassName('cell')[i].style.color = "#fff";
+  }
+  var show = reveal;
+  if(show == 1){
+    document.getElementsByClassName('cell')[i].style.color = '#000';
+  }
+}
+wordHide();
+//Function that adds a class name
+function addClass(element, classname) {
+  element.className = 'cells ' + classname;
+}
+//Funtion that changes the alphabet array for when a letter is inputed by the user
+function changeAlphabet (letter) {
+  document.getElementsByClassName('cells')[letter].style.backgroundColor = 'red';
+  document.getElementsByClassName('cells')[letter].style.color = '#fff';
+}
+//Loop to add an Eventlistener to each element in order to change the box to red when clicked, informing the user this letter has been used.
+for(var i = 0; i < alphabet.length; i++){
+  document.getElementsByClassName('cells')[i].addEventListener('click', function(){
+    addClass(this, 'red');
+    this.style.color = '#fff';
+  });
+}
+//Function that searches for the letter inputed by the user in the alphabet array
+var myLetter;
+function letterSearch (letter){
+  for(var i = 0; i < alphabet.length; i++){
+    if(letter == alphabet[i]){
+      myLetter = i;
+    }
+  }
+}
+//Variable to save the letter pressed by the user
 var thisLetter;
 //Function to determine which letter was chosen by the user
 function getChar (evt) {
@@ -54,18 +114,16 @@ function getChar (evt) {
     var charStr = String.fromCharCode(charCode);
     thisLetter = charStr;}
 //Function to check if the inputed letter is part of the game word
-function checkLetter(){
+function gameOn(){
   getChar();
+  letterSearch(thisLetter.toUpperCase());
+  changeAlphabet(myLetter);
   var letter = randomSong.split('');
-  var i = 0;
-  while(i<randomSong.length){
-    if(thisLetter==letter[i]){
-      //Stuff happens
-      break;
-    }
-    else //Stuff Happens
-    i++;
+  var attempts = 6;
+  while(attempts){
+    checkLetter(thisLetter);
   }
 }
 //Eventlistener for the page
-document.addEventListener('keypress', checkLetter);
+document.addEventListener('keypress', gameOn);
+document.addEventListener('click', gameOn);
